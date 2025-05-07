4799656125c061053a2d36fb3569ac3f8a4743eb
jfcampos5 <jfcampos5@hotmail.com>
2025-05-07 18:26:41 +0000

    fix: definir _id como chave primária nos models para compatibilidade com MongoDB e DRF

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,7 +1,7 @@
 from djongo import models
 
 class User(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.ObjectIdField(primary_key=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=128)
@@ -9,14 +9,14 @@ class User(models.Model):
         db_table = 'users'
 
 class Team(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.ObjectIdField(primary_key=True)
     name = models.CharField(max_length=100)
     members = models.JSONField(default=list)  # lista de e-mails dos membros
     class Meta:
         db_table = 'teams'
 
 class Activity(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.ObjectIdField(primary_key=True)
     user = models.CharField(max_length=100)
     activity_type = models.CharField(max_length=50)
     duration = models.IntegerField()
@@ -25,14 +25,14 @@ class Activity(models.Model):
         db_table = 'activity'
 
 class Leaderboard(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.ObjectIdField(primary_key=True)
     user = models.CharField(max_length=100)
     points = models.IntegerField()
     class Meta:
         db_table = 'leaderboard'
 
 class Workout(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.ObjectIdField(primary_key=True)
     user = models.CharField(max_length=100)
     workout_type = models.CharField(max_length=50)
     details = models.TextField()
